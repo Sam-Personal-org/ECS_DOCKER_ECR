c26137f66bcf7063079ca407c3e940dbfa7a275e
Sameem Mohamed Rafeeq <sameem.aws21@gmail.com>
2026-01-28 16:33:41 +0530

    Add IAM role and policy for ECS execution

--- terraformcode/main.tf
+++ terraformcode/main.tf
@@ -123,6 +123,30 @@ output "ecs_cluster_id" {
     value = aws_ecs_cluster.test_ecs_cluster.id
 }
 
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecsTaskExecutionRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+output "ecs_execution_role_arn" {
+  value = aws_iam_role.ecs_execution_role.arn
+}
+
+
 resource "aws_ecs_task_definition" "test_task_definition" {
     family                   = "test_task_definition"
     network_mode             = "awsvpc"
@@ -130,7 +154,7 @@ resource "aws_ecs_task_definition" "test_task_definition" {
     cpu                      = "1024"
     memory                   = "2048"
     
-    execution_role_arn = var.execution_role_arn
+    execution_role_arn = aws_iam_role.ecs_execution_role.arn
 
     container_definitions = jsonencode([
     {
@@ -162,3 +186,5 @@ resource "aws_ecs_service" "test_ecs_service" {
     }
     depends_on = [aws_ecs_cluster.test_ecs_cluster]
 }
+
+
