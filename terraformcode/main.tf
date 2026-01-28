4224454ab47a8d3edc976df4ca0663dd98c54e94
Sameem Mohamed Rafeeq <sameem.aws21@gmail.com>
2026-01-28 17:12:08 +0530

    adding lifecycle

--- terraformcode/main.tf
+++ terraformcode/main.tf
@@ -128,6 +128,9 @@ upper = false
 special = false 
 }
 resource "aws_iam_role" "ecs_execution_role" {
+lifecycle {
+    create_before_destroy = true
+  }
 
   name = "ecsTaskExecutionRole-${random_string.suffix.result}"
   assume_role_policy = jsonencode({
